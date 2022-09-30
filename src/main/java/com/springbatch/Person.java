92b3de73c7a2d8d69de938293f961c184fca6ee6
manojsuthar-capiter <manoj.suthar@capiter.net>
2022-09-30 19:35:12 +0530

    Initial commit

---
+++ src/main/java/com/springbatch/Person.java
@@ -0,0 +1,13 @@
+package com.springbatch;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private String lastName;
+    private String firstName;
+}
